0951002e7a5a8a8927e8800115f71f2e8b2e3e9b
Enrique Guerrero <bgm94@hotmail.com>
2023-02-14 01:02:46 -0600

    ✨ Add home view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,54 @@
 'use client';
 
 import { PokemonCard } from '@/components/cards';
+import ENDPOINTS from '@/constants/endpoints';
 import TIME from '@/constants/time';
-import { getPokemonList } from '@/services';
-import { Layouts, Typography } from '@/styles';
-import Link from 'next/link';
+import { getPokedexList, getPokemonsByPokedex } from '@/services/pokedex';
+import { Layouts } from '@/styles';
+import Button from '@/styles/button';
 import { useState } from 'react';
 import { useQuery } from 'react-query';
 
 export default function Home() {
-  const [pageSize, setPageSize] = useState(9);
-  const [page, setPage] = useState(0);
+  const [pokemonRegionUrl, setPokemonRegionUrl] = useState(ENDPOINTS.POKEDEXES.KANTO);
+
+  const { data: pokemonRegionList } = useQuery({
+    queryKey: ['pokedexes'],
+    queryFn: ({ signal }) => getPokedexList({ abortSignal: signal }),
+    staleTime: TIME.ONE_DAY,
+  });
 
   const { data: pokemonList } = useQuery({
-    queryKey: ['pokemons', page, pageSize],
-    queryFn: ({ signal }) => getPokemonList({ limit: pageSize, offset: page * pageSize, abortSignal: signal }),
+    queryKey: ['pokemons', pokemonRegionUrl],
+    queryFn: ({ signal }) => getPokemonsByPokedex({ abortSignal: signal, pokedexUrl: pokemonRegionUrl }),
+    staleTime: TIME.ONE_DAY,
   });
+
+  const handlePokemonRegionClick = (url: string) => {
+    setPokemonRegionUrl(url);
+  };
   return (
     <main>
-      <Layouts.Grid>
-        {pokemonList ? (
-          pokemonList.map((element) => {
-            return (
-              <PokemonCard
-                key={element.data.name}
-                name={element.data.name}
-                src={element.data.sprites.other['official-artwork']?.front_default}
-              />
-            );
-          })
-        ) : (
-          <div>Loading...</div>
-        )}
-      </Layouts.Grid>
-      <Link href={'./pokemonName'}>Link</Link>
+      <Layouts.Flex direction="row" css={{ alignItems: 'flex-start', gap: '1rem' }}>
+        <Layouts.Flex direction="column" css={{ gap: '1rem' }}>
+          {pokemonRegionList
+            ? pokemonRegionList.data.results.map((pokemonRegion) => (
+                <Button.Primary key={pokemonRegion.name} onClick={() => handlePokemonRegionClick(pokemonRegion.url)}>
+                  {pokemonRegion.name}
+                </Button.Primary>
+              ))
+            : 'loading...'}
+        </Layouts.Flex>
+        <Layouts.Grid layout="pokemonList">
+          {pokemonList ? (
+            pokemonList.map((element) => {
+              return <PokemonCard key={element.data.name} pokemonDetail={element.data} />;
+            })
+          ) : (
+            <div>Loading...</div>
+          )}
+        </Layouts.Grid>
+      </Layouts.Flex>
     </main>
   );
 }
