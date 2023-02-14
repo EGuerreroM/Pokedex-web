d370b610ceab05f97f1323a9c814cf7f8b657f32
Enrique Guerrero <bgm94@hotmail.com>
2023-02-14 01:00:14 -0600

    💬 Add pokemon details route

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -2,8 +2,13 @@ const IMAGES = {
   PLACEHOLDER: '/images/pokeball.png',
 };
 
+const POKEMONS = {
+  DETAIL: (name: string) => `pokemons/${name}`,
+};
+
 const ROUTES = {
   IMAGES,
+  POKEMONS,
 };
 
 export default ROUTES;
