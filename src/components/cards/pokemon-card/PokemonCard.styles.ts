918c8ba39fb6722ad639a59cc090da188b512d8e
Enrique Guerrero <bgm94@hotmail.com>
2023-02-14 01:02:09 -0600

    💄 Add card component

--- src/components/cards/pokemon-card/PokemonCard.styles.ts
+++ src/components/cards/pokemon-card/PokemonCard.styles.ts
@@ -1,9 +1,23 @@
 import { styled } from '@/config/stitches.config';
 
 export const CardContainer = styled('div', {
-  position: 'relative',
-  borderRadius: '0.25rem',
-  backgroundColor: '$hiContrast',
+  display: 'flex',
+  flexDirection: 'column',
+  borderRadius: '4px',
+  backgroundColor: '$loContrast',
+  boxShadow: '0px 1px 13px rgba(0,0,0,0.1);',
 });
 
-export const CardImage = styled('img', {});
+export const CardImageContainer = styled('div', {
+  position: 'relative',
+  height: '160px',
+  width: '100%',
+});
+
+export const CardBody = styled('div', {
+  padding: '1rem',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+});
--- src/styles/layouts.ts
+++ src/styles/layouts.ts
@@ -11,8 +11,8 @@ const Flex = styled('div', {
       },
       column: {
         flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
       },
     },
   },
@@ -20,8 +20,16 @@ const Flex = styled('div', {
 
 const Grid = styled('div', {
   display: 'grid',
+  width: '100%',
   gridTemplateColumns: 'repeat(3, 1fr)',
   gap: '1rem',
+  variants: {
+    layout: {
+      pokemonList: {
+        gridTemplateColumns: 'repeat(6, 178px)',
+      },
+    },
+  },
 });
 
 const Layouts = {
