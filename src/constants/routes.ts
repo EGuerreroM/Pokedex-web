c00e200934c17e9ba2cb916041bb27cea9c082f1
Enrique Guerrero <bgm94@hotmail.com>
2023-02-19 15:19:29 -0600

    💬 Add Routes

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -2,12 +2,21 @@ const IMAGES = {
   PLACEHOLDER: '/images/pokeball.png',
 };
 
+const HOME = '/';
+
 const POKEMONS = {
+  LIST: 'pokemons',
   DETAIL: (name: string) => `pokemons/${name}`,
 };
 
+const BERRIES = {
+  LIST: 'berries',
+};
+
 const ROUTES = {
+  HOME,
   IMAGES,
   POKEMONS,
+  BERRIES,
 };
 export default ROUTES;
