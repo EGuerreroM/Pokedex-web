ab743780fecbd1505e34f39b0449c91a11188904
rod <rodrigovalladares1@gmail.com>
2023-03-13 23:11:24 -0600

    set movie db variables

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -1,3 +1,5 @@
+import ENVIROMENT from "./enviroment";
+
 const POKEMONS = {
   DATA: 'https://pokeapi.co/api/v2/pokemon',
   DETAIL: (name: string) => `https://pokeapi.co/api/v2/pokemon/${name}`,
@@ -10,9 +12,14 @@ const POKEDEXES = {
   KANTO: 'https://pokeapi.co/api/v2/pokedex/kanto',
 };
 
+const POKEMONSMOVIES = {
+  DATA: `https://api.themoviedb.org/3/search/movie?api_key=${ENVIROMENT.APIKEY}&language=en-US&query=pokemon&page=1&include_adult=false`
+}
+
 const ENDPOINTS = {
   POKEMONS,
   POKEDEXES,
+  POKEMONSMOVIES,
 };
 
 export default ENDPOINTS;
---
+++ src/constants/enviroment.ts
@@ -0,0 +1,6 @@
+const ENVIROMENT = {
+  APIKEY: 'c854ef0fbd8ed619f4db8ffb2fa5d84a',
+  IMAGEURL: (image: string) => `https://image.tmdb.org/t/p/w500/${image}`,
+};
+
+export default ENVIROMENT;
