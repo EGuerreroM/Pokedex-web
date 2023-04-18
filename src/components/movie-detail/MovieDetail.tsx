674a056f6b5f6776e00883cb8761850c9672a939
Srsanchxz <79011986+Srsanchxz@users.noreply.github.com>
2023-04-17 21:36:20 -0600

    add links

--- src/components/movie-detail/MovieDetail.tsx
+++ src/components/movie-detail/MovieDetail.tsx
@@ -3,7 +3,8 @@
 import ENVIROMENT from '@/constants/enviroment';
 import ROUTES from '@/constants/routes';
 import { IPokemonMovie } from '@/interfaces/API';
-import { Box, Heading, Image, Stack, Text, Button } from '@chakra-ui/react';
+import { Link, Box, Heading, Image, Stack, Text, Button } from '@chakra-ui/react';
+import { ExternalLinkIcon } from '@chakra-ui/icons';
 
 type MovieDetailProps = {
   movieDetail: IPokemonMovie | null;
@@ -31,15 +32,33 @@ const MovieDetail = (props: MovieDetailProps) => {
           <Text>{movieDetail?.overview}</Text>
 
           <Stack spacing={4} direction="row" align="center">
-            <Button colorScheme="teal" size="sm">
-              Prime Video
-            </Button>
-            <Button colorScheme="teal" size="sm">
-              Netflix
-            </Button>
-            <Button colorScheme="teal" size="sm">
-              HBO Max
-            </Button>
+            <Link
+              href="https://play.hbomax.com/signIn"
+              isExternal
+              paddingX="8px"
+              paddingY="6px"
+              background="#319795"
+              borderRadius="10px">
+              HBO max <ExternalLinkIcon mx="2px" />
+            </Link>
+            <Link
+              href="https://www.netflix.com/sv/login"
+              isExternal
+              paddingX="8px"
+              paddingY="6px"
+              background="#319795"
+              borderRadius="10px">
+              Netflix <ExternalLinkIcon mx="2px" />
+            </Link>
+            <Link
+              href="https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fna.primevideo.com%2Fauth%2Freturn%2Fref%3Dav_auth_ap%3F_t%3Dsg1kIRD-Jz1cKs6NOs0tI9ZwHvvGzLrH3UyiZHJ9z0ffUAAAAAQAAAABkPgpFcmF3AAAAAPgWC9WfHH8iB-olH_E9xQ%26location%3Dhttps%3A%2F%2Fwww.primevideo.com%2Foffers%2Fnonprimehomepage%3Fref_%253Ddvm_pds_brd_sv_hk_s_g_mkw_saWUWq8BS-dc_pcrid_645960032293%2526mrntrk%253Dslid__pgrid_42306244928_pgeo_1012108_x__adext__ptid_kwd-2850171361&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&accountStatusPolicy=P1&openid.assoc_handle=amzn_prime_video_sso_us&openid.mode=checkid_setup&siteState=134-2731506-3700367&language=en_US&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0"
+              isExternal
+              paddingX="8px"
+              paddingY="6px"
+              background="#319795"
+              borderRadius="10px">
+              Prime Video <ExternalLinkIcon mx="2px" />
+            </Link>
           </Stack>
         </Stack>
       </Stack>
